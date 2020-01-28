bf0364d5976210e86527000e958046caf4ff37a4
Unknown <danielhey@outlook.com>
2020-01-28 12:27:31 +1100

    fixing links

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ import os
 from setuptools import setup, Extension
 
 # Load the __version__ variable without importing the package already
-exec(open('maelstrom/version.py').read())
+exec(open("maelstrom/version.py").read())
 
 # Get dependencies
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     install_requires = f.read().splitlines()
 
-setup(name='maelstrom',
-      version=__version__,
-      description="A package to model the orbits of pulsating stars in binaries",
-      long_description=open('README.rst').read(),
-      license='MIT',
-      package_dir={
-            'maelstrom': 'maelstrom',},
-      packages=['maelstrom'],
-      install_requires=install_requires,
-      url='https://github.com/danielhey/maelstrom',
-      include_package_data=True,
+setup(
+    name="maelstrom",
+    version=__version__,
+    description="A package to model the orbits of pulsating stars in binaries",
+    long_description=open("README.md").read(),
+    license="MIT",
+    package_dir={"maelstrom": "maelstrom",},
+    packages=["maelstrom"],
+    install_requires=install_requires,
+    url="https://github.com/danhey/maelstrom",
+    include_package_data=True,
 )
