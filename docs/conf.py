ae7f09236ca15a9058806cb2bcabe33d3df3e3c5
Daniel <38233719+danhey@users.noreply.github.com>
2020-01-29 16:39:46 +1100

    analytics

--- docs/conf.py
+++ docs/conf.py
@@ -101,7 +101,9 @@ html_theme = "sphinx_rtd_theme"
 # documentation.
 #
 # html_theme_options = {}
-
+html_theme_options = {
+    'analytics_id': 'UA-144184844-2'
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
